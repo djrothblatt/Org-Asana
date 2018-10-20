45301ef11efff0299bb7560a0bf8c660538ab3b7
Daniel Rothblatt <drothblatt@tworiverstrading.com>
2018-10-19 22:10:47 -0400

    Fetch task trees
    Because Asana tasks can have subtasks, they are naturally modelled with
    a tree-like data structure. The Asana search API makes it very easy to
    fetch only the tasks that aren't subtasks, which in turn makes it easy
    to build a forest of tasks and their subtasks.

--- org-asana.el
+++ org-asana.el
@@ -17,7 +17,7 @@
             `(("Authorization" . ,(concat "Bearer " org-asana/token)))))
         (url-retrieve-synchronously (concat "https://app.asana.com/api/1.0" endpoint)))
    (goto-char (1+ url-http-end-of-headers))
-   (org-asana/data (json-read-object))))
+   (seq-into (org-asana/data (json-read-object)) 'list)))
 
 (defun org-asana/about-me ()
   (org-asana/request "/users/me"))
@@ -29,25 +29,37 @@
 
 (cl-defmacro org-asana/with-me (&body body)
   "Bind anaphoric value `me' in BODY."
-  `(let ((me (alist-get 'data (org-asana/set-me!))))
+  `(let ((me (org-asana/set-me!)))
      ,@body))
 
 (defun org-asana/my-workspaces ()
   "Get all workspaces associated with org-asana/me."
   (org-asana/with-me
-   (alist-get 'workspaces me)))
+   (seq-into
+    (alist-get 'workspaces org-asana/me)
+    'list)))
 
-(defun org-asana/my-tasks (workspace)
-  "Fetch tasks for `org-asana/me' in WORKSPACE."
+(defun org-asana/root-tasks (workspace)
+  "Fetch tasks that aren't subtasks for `org-asana/me' in WORKSPACE."
   (org-asana/with-me
    (let* ((assignee (alist-get 'gid me))
           (workspace-id (alist-get 'gid workspace))
           (endpoint (concat "/workspaces/" workspace-id "/tasks/search"))
-          (query-string (url-build-query-string `(("assignee.any" ,assignee)))))
+          (query-string (url-build-query-string `(("assignee.any" ,assignee) ("is_subtask" "false")))))
      (org-asana/request (concat endpoint "?" query-string)))))
 
 (defun org-asana/subtasks (task)
   "Fetch subtasks of TASK."
   (let-alist task
     (org-asana/request (concat "/tasks/" .gid "/subtasks"))))
+
+(defun org-asana/task-tree (task)
+      (acons 'subtasks
+             (mapcar #'org-asana/task-tree
+                     (org-asana/subtasks task))
+             task))
+
+(defun org-asana/task-forest (tasks)
+  "Get task trees for every task in TASKS."
+  (mapcar #'org-asana/task-tree tasks))
 
